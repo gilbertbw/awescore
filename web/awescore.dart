e2c88984bfea658dee9bf5a8166737b35d9ffd96
Gilbert Bishop-White <gilbert@bw.gg>
2014-01-25 21:20:16 +0000

    Added user class to client

--- web/awescore.dart
+++ web/awescore.dart
@@ -4,6 +4,11 @@ void main() {
   querySelector("#sample_text_id")
     ..text = "Click me!"
     ..onClick.listen(reverseText);
+  
+  User gilbert = new User("gilbertbw", 12.34, 3.42);
+  
+  print(gilbert.score);
+  
 }
 
 void reverseText(MouseEvent event) {
@@ -13,4 +18,23 @@ void reverseText(MouseEvent event) {
     buffer.write(text[i]);
   }
   querySelector("#sample_text_id").text = buffer.toString();
+}
+
+class User {
+  final String username;
+  int _score;
+  int _change;
+  
+  User(this.username, score, change) {
+    num scoreDec = score*100;
+    this._score = scoreDec.toInt();
+    
+    num changeDec = change*100;
+    this._change = changeDec.toInt();
+  }
+  
+  num get score {
+    num score = this._score.toDouble();
+    return score/100;
+  }
 }
